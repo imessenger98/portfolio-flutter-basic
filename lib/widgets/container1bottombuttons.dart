619d1a91f4f5aad534f830eb7db42ab2fb393b9a
MUHAMMED YAZEEN <27795050+messenger-1012@users.noreply.github.com>
2021-07-08 23:54:12 +0530

    1st screen completed

--- lib/widgets/container1bottombuttons.dart
+++ lib/widgets/container1bottombuttons.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/widgets/secondscreen/secondscreenm.dart';
 
 class BottomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 100),
-      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(100),
           border: Border.all(width: 3, color: Colors.white)),
-      child: Icon(
-        Icons.arrow_forward_ios,
-        color: Colors.white,
-        size: 30,
+      child: TextButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SecondScreen()));
+        },
+        child: Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.white,
+          size: 30,
+        ),
       ),
     );
   }
