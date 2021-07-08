68e3ec93a409fa9a3c22649f44a72e7585cb9bde
MUHAMMED YAZEEN <27795050+messenger-1012@users.noreply.github.com>
2021-07-09 00:59:08 +0530

    rotate fixed

--- lib/widgets/firstscreen/coloumn1.dart
+++ lib/widgets/firstscreen/coloumn1.dart
@@ -6,82 +6,87 @@ import 'package:url_launcher/url_launcher.dart';
 class Coloumn1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("images/mydes.png"),
-          colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.5), BlendMode.dstATop),
-          fit: BoxFit.cover,
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/mydes.png"),
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5), BlendMode.dstATop),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: CircleAvatar(
-                radius: 90,
-                backgroundImage: AssetImage('images/yazeenan.jpg'),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                "MUHAMMED YAZEEN AN",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  letterSpacing: 4,
-                  fontFamily: "Wallpoet-Regular",
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: Text(
-                "SOFTWARE DEVELOPER",
-                style: TextStyle(
-                  color: Colors.red[900],
-                  fontWeight: FontWeight.w900,
-                  fontSize: 12,
-                  letterSpacing: 1.5,
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.personal_video,
-                    color: Colors.white,
-                    size: 20,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top:10),
+                  child: CircleAvatar(
+                    radius: 90,
+                    backgroundImage: AssetImage('images/yazeenan.jpg'),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      _launchURL(n);
-                    },
-                    child: Text(
-                      "  muhammedyazeenan.live",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 15,
-                        letterSpacing: 2,
-                      ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text(
+                    "MUHAMMED YAZEEN AN",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      letterSpacing: 4,
+                      fontFamily: "Wallpoet-Regular",
                     ),
                   ),
-                ],
-              ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "SOFTWARE DEVELOPER",
+                    style: TextStyle(
+                      color: Colors.red[900],
+                      fontWeight: FontWeight.w900,
+                      fontSize: 12,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.personal_video,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          _launchURL(n);
+                        },
+                        child: Text(
+                          "  muhammedyazeenan.live",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                            letterSpacing: 2,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ColoumnButton(),
+                BottomButton()
+              ],
             ),
-            ColoumnButton(),
-            BottomButton()
-          ],
+          ),
         ),
       ),
     );
--- lib/widgets/firstscreen/container1bottombuttons.dart
+++ lib/widgets/firstscreen/container1bottombuttons.dart
@@ -4,20 +4,22 @@ import 'package:portfolio/widgets/secondscreen/secondscreenm.dart';
 class BottomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 100),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          border: Border.all(width: 3, color: Colors.white)),
-      child: TextButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SecondScreen()));
-        },
-        child: Icon(
-          Icons.arrow_forward_ios,
-          color: Colors.white,
-          size: 30,
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 80,bottom: 30),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            border: Border.all(width: 3, color: Colors.white)),
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SecondScreen()));
+          },
+          child: Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.white,
+            size: 30,
+          ),
         ),
       ),
     );
