ac9c9c4cd91a51d46655f2521b4434af9717a75d
MUHAMMED YAZEEN <27795050+messenger-1012@users.noreply.github.com>
2021-07-09 12:29:04 +0530

    started making new changes to the second screen

--- lib/widgets/secondscreen/secondscreenm.dart
+++ lib/widgets/secondscreen/secondscreenm.dart
@@ -5,11 +5,36 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("page under developement")],
+          child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          image: DecorationImage(
+            image: AssetImage("images/mydes.png"),
+            // colorFilter: ColorFilter.mode(
+            //   Colors.black.withOpacity(0.8),
+            //   BlendMode.dstATop,
+            // ),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "page under developement",
+                style: TextStyle(
+                  letterSpacing: 2,
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
